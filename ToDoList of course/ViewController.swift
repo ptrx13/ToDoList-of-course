24aa80b67575bfd71156ceede90016d4ddcdf365
ptrx13 <patriarxnik@gmail.com>
2022-05-12 22:23:58 +0300

    Добавление методов делегата и дата сорс

--- ToDoList of course/ViewController.swift
+++ ToDoList of course/ViewController.swift
@@ -7,13 +7,70 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ToDoListViewController: UITableViewController {
+    
+    var itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+    
+    let defaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        if let items = defaults.array(forKey: "ToDoListAttay") as? [String] {
+            itemArray = items
+        }
+        
     }
-
-
+    
+    // MARK: - TableView DataSource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+    
+    // MARK: - TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //print(itemArray[indexPath.row])
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+    }
+    
+    // MARK: - Add item
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Новая задача", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Добавить", style: .default) { (action) in
+            self.itemArray.append(textField.text ?? "")
+            self.defaults.set(self.itemArray, forKey: "ToDoListAttay")
+            self.tableView.reloadData()
+        }
+        
+        alert.addTextField { (alertTF) in
+            alertTF.placeholder = "Добавить новую задачу"
+            textField = alertTF
+        }
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
+
+
 
